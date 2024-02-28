5832762b58f2d6172517f8714a66c94e92c85bd5
LsAnndes <160362727+LsAnndes@users.noreply.github.com>
2024-02-28 18:39:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,38 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     var staticImage = document.getElementById('staticImage');
     var gif = document.getElementById('gif');
     gif.style.display = 'none';
-
-    const buttons = document.querySelectorAll('.button');
-
-    function handleButtonClick(event) {
-        buttons.forEach(button => button.classList.remove('clicked'));
-        event.target.classList.add('clicked');
-    }
-
-    function preloadImages() {
-        const images = [
-            'sun.gif',
-            'sun2.gif',
-            'sun3.gif',
-            'sun4.gif',
-            'sun5.gif',
-            'sun6.gif'
-        ];
-
-        images.forEach(image => {
-            const img = new Image();
-            img.src = image;
-        });
-    }
-
-    preloadImages();
-
-    buttons.forEach(button => {
-        button.addEventListener('click', function() {
-            handleButtonClick(event);
-            changeGif(event.target.getAttribute('data-current-src'));
-        });
-    });
 });
 
 function changeGif(src) {
@@ -43,3 +11,13 @@ function changeGif(src) {
     gif.src = src;
     gif.style.display = 'block';
 }
+
+const buttons = document.querySelectorAll('.button');
+
+function handleButtonClick(event) {
+    buttons.forEach(button => button.classList.remove('clicked'));
+    event.target.classList.add('clicked');
+}
+buttons.forEach(button => {
+    button.addEventListener('click', handleButtonClick);
+});
